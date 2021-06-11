1b202a5672a27611ac50acd08c2913a8aa3b3962
eunchae2000 <tldjs3651@naver.com>
2021-06-12 03:48:15 +0900

    deepmlp 코드 수정

--- deepmlp.py
+++ deepmlp.py
@@ -1,58 +1,41 @@
 import numpy as np
 import pandas as pd
-import tensorflow as tf
+from sklearn.neural_network import MLPClassifier
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
 
-TRAIN_DATA_PATH = "C:\Users\coco1\Desktop\AI시스템설계\train.csv"
-TEST_DATA_PATH = "C:\Users\coco1\Desktop\AI시스템설계\test.csv"
+# csv 형식으로 된 파일 가져오기
+mnist_train = "/content/train.csv"
+mnist_test = "/content/test.csv"
 
-X_train = np.array(raw_train_data.drop("label", axis=1)).astype(float)
+# csv 형식으로 된 파일 읽어오기
+raw_train_data = pd.read_csv(mnist_train)
+raw_test_data = pd.read_csv(mnist_test)
+
+x_train = np.array(raw_train_data.drop("label", axis=1)).astype(float)
 y_train = np.array(raw_train_data["label"])
 
-X_test = np.array(raw_test_data.drop("id", axis=1)).astype(float)
+x_test = np.array(raw_test_data.drop("id", axis=1)).astype(float)
 y_test = np.array(raw_test_data["id"])
 
-from sklearn.model_selection import train_test_split
+n_input = 784
+n_hidden1 = 1024
+n_hidden2 = 512
+n_hidden3 = 512
+n_hidden4 = 512
+n_output = 10
 
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.15, random_state=42)
+mlp = Sequential()
+mlp.add(Dense(units=n_hidden1, activation='tanh',input_shape=(n_input,), kernel_initializer='random_uniform', bias_initializer='zero'))
+mlp.add(Dense(units=n_hidden2, activation='tanh', kernel_initializer='random_uniform', bias_initializer='zeros'))
+mlp.add(Dense(units=n_hidden3, activation='tanh', kernel_initializer='random_uniform', bias_initializer='zeros'))
+mlp.add(Dense(units=n_hidden4, activation='tanh', kernel_initializer='random_uniform', bias_initializer='zeros'))
+mlp.add(Dense(units=n_output, activation='tanh', kernel_initializer='random_uniform', bias_initializer='zeros'))
 
+mlp.compile(loss='mse', optimizer=Adam(learning_rate=0.001), metrics=['accuracy'])
+hist=mlp.fit(x_train, y_train, batch_size=128, epochs=30, validation_data=(x_test, y_test), verbose=2)
 
-print(f"X_train Shape: {X_train.shape}, y_train Shape: {y_train.shape}")
-print(f"X_val Shape: {X_val.shape}, y_val Shape: {y_val.shape}")
-print(f"X_test Shape: {X_test.shape}, y_test Shape: {y_test.shape}")
-
-np.random.seed(42)
-tf.random.set_seed(42)
-
-mlp_model = Sequential()
-mlp_model = tf.keras.models.Sequential([
-                tf.keras.layers.Flatten(input_shape=[28, 28], name="input_layer"),
-                tf.keras.layers.Dense(1024, activation="relu", name="hidden_layer1"),
-                tf.keras.layers.Dense(512, activation="relu", name="hidden_layer2"),
-                tf.keras.layers.Dense(512, activation="relu", name="hidden_layer3"),
-                tf.keras.layers.Dense(512, activation="relu", name="hidden_layer4"),
-                tf.keras.layers.Dense(10, activation="softmax", name="output_layer")
-])
-
-# compile MLP model
-mlp_model.compile(loss="mse",
-                  optimizer=Adam(learning_rate=0.001),
-                  metrics=["accuracy"])
-
-# display a breakdown of the MLP model
-mlp_model.summary()
-
-val_stop = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=10, restore_best_weights=True)
-
-# train the MLP
-training_progress = mlp_model.fit(X_train, y_train, epochs=1000,
-                                  batch_size=128,
-                                  validation_data=(X_val, y_val),
-                                  verbose = 2,
-                                  callbacks=[val_stop])
-
-res = mlp_model.evaluate(X_test, y_test, verbose = 0)
-print("Accuracy is ", res[1]*100)
+res = mlp.evaluate(x_test, y_test, verbose=0)
+print("Accuracy is ", res[1]*100, "%")
